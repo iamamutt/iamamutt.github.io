503b4a8a35de868fafc6c0ba7d8dd09d6e185525
Joseph Burling <josephburling@gmail.com>
2023-08-23 14:13:09 -0500

    feat: :test_tube: add test page

---
+++ README.md
@@ -0,0 +1,13 @@
+# GitHub Pages Personal Site
+
+```
+iamamutt.github.io/
+├── content
+│   └── pages
+│       └── home.md
+├── output
+├── tasks.py
+├── Makefile
+├── pelicanconf.py       # Main settings file
+└── publishconf.py       # Settings to use when ready to publish
+```
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
-AUTHOR = 'Joseph Burling'
-SITENAME = 'Joseph Burling'
-SITEURL = ''
+AUTHOR = "Joseph M. Burling"
+SITENAME = "Joseph Burling"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'America/Chicago'
+TIMEZONE = "America/Chicago"
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = "English"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -16,16 +16,22 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+GITHUB_PAGES_BRANCH = "gh-pages"
